1156dc120dea844c72f6df949a41f456ad7fab2b
Greg Holt <gregory_holt@icloud.com>
2016-03-25 19:30:18 +0000

    Prototype for using a ring service

--- api/valuereplstore_GEN_.go
+++ api/valuereplstore_GEN_.go
@@ -3,16 +3,22 @@ package api
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
 	"sync"
 	"time"
 
+	"github.com/gholt/flog"
 	"github.com/gholt/ring"
 	"github.com/gholt/store"
+	pbTODO "github.com/pandemicsyn/oort/api/valueproto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
 type ReplValueStore struct {
+	logError                   func(string, ...interface{})
 	logDebug                   func(string, ...interface{})
 	logDebugOn                 bool
 	addressIndex               int
@@ -21,8 +27,12 @@ type ReplValueStore struct {
 	failedConnectRetryDelay    int
 	grpcOpts                   []grpc.DialOption
 
-	ringLock sync.RWMutex
-	ring     ring.Ring
+	ringLock           sync.RWMutex
+	ring               ring.Ring
+	ringCachePath      string
+	ringServer         string
+	ringServerGRPCOpts []grpc.DialOption
+	ringServerExitChan chan struct{}
 
 	storesLock sync.RWMutex
 	stores     map[string]*replValueStoreAndTicketChan
@@ -36,6 +46,7 @@ type replValueStoreAndTicketChan struct {
 func NewReplValueStore(c *ReplValueStoreConfig) *ReplValueStore {
 	cfg := resolveReplValueStoreConfig(c)
 	rs := &ReplValueStore{
+		logError:                   cfg.LogError,
 		logDebug:                   cfg.LogDebug,
 		logDebugOn:                 cfg.LogDebug != nil,
 		addressIndex:               cfg.AddressIndex,
@@ -44,9 +55,26 @@ func NewReplValueStore(c *ReplValueStoreConfig) *ReplValueStore {
 		failedConnectRetryDelay:    cfg.FailedConnectRetryDelay,
 		grpcOpts:                   cfg.GRPCOpts,
 		stores:                     make(map[string]*replValueStoreAndTicketChan),
+		ringServer:                 cfg.RingServer,
+		ringServerGRPCOpts:         cfg.RingServerGRPCOpts,
+		ringCachePath:              cfg.RingCachePath,
+	}
+	if rs.logError == nil {
+		rs.logError = flog.Default.ErrorPrintf
 	}
 	if rs.logDebug == nil {
 		rs.logDebug = func(string, ...interface{}) {}
+	}
+	if rs.ringCachePath != "" {
+		if fp, err := os.Open(rs.ringCachePath); err != nil {
+			rs.logDebug("replValueStore: error loading cached ring %q: %s", rs.ringCachePath, err)
+		} else if r, err := ring.LoadRing(fp); err != nil {
+			fp.Close()
+			rs.logDebug("replValueStore: error loading cached ring %q: %s", rs.ringCachePath, err)
+		} else {
+			fp.Close()
+			rs.ring = r
+		}
 	}
 	return rs
 }
@@ -59,9 +87,28 @@ func (rs *ReplValueStore) Ring() ring.Ring {
 }
 
 func (rs *ReplValueStore) SetRing(r ring.Ring) {
+	if r == nil {
+		return
+	}
 	rs.ringLock.Lock()
+	if rs.ringCachePath != "" {
+		dir, name := path.Split(rs.ringCachePath)
+		fp, err := ioutil.TempFile(dir, name)
+		if err != nil {
+			rs.logDebug("replValueStore: error caching ring %q: %s", rs.ringCachePath, err)
+		} else if err := r.Persist(fp); err != nil {
+			fp.Close()
+			os.Remove(fp.Name())
+			rs.logDebug("replValueStore: error caching ring %q: %s", rs.ringCachePath, err)
+		} else {
+			fp.Close()
+			if err := os.Rename(fp.Name(), rs.ringCachePath); err != nil {
+				os.Remove(fp.Name())
+				rs.logDebug("replValueStore: error caching ring %q: %s", rs.ringCachePath, err)
+			}
+		}
+	}
 	rs.ring = r
-	rs.ringLock.Unlock()
 	var currentAddrs map[string]struct{}
 	if r != nil {
 		nodes := r.Nodes()
@@ -92,6 +139,7 @@ func (rs *ReplValueStore) SetRing(r ring.Ring) {
 			}
 		}
 	}
+	rs.ringLock.Unlock()
 }
 
 func (rs *ReplValueStore) storesFor(ctx context.Context, keyA uint64) ([]*replValueStoreAndTicketChan, error) {
@@ -176,11 +224,118 @@ func (rs *ReplValueStore) storesFor(ctx context.Context, keyA uint64) ([]*replVa
 	return ss, nil
 }
 
+func (rs *ReplValueStore) ringServerConnector(exitChan chan struct{}) {
+	sleeperTicks := 2
+	sleeperTicker := time.NewTicker(time.Second)
+	sleeper := func() {
+		for i := sleeperTicks; i > 0; i-- {
+			select {
+			case <-exitChan:
+				break
+			case <-sleeperTicker.C:
+			}
+		}
+		if sleeperTicks < 60 {
+			sleeperTicks *= 2
+		}
+	}
+	for {
+		select {
+		case <-exitChan:
+			break
+		default:
+		}
+		ringServer := rs.ringServer
+		if ringServer == "" {
+			// TODO: Use DNS to resolve the ringServer.
+			err := errors.New("resolving ring service not implemented yet.")
+			rs.logError("replValueStore: error resolving ring service: %s", err)
+			sleeper()
+			continue
+		}
+		conn, err := grpc.Dial(ringServer, rs.ringServerGRPCOpts...)
+		if err != nil {
+			rs.logError("replValueStore: error connecting to ring service %s: %s", ringServer, err)
+			sleeper()
+			continue
+		}
+		// TODO: This isn't the right stream type.
+		stream, err := pbTODO.NewValueStoreClient(conn).StreamLookup(context.Background())
+		if err != nil {
+			rs.logError("replValueStore: error creating stream with ring service %s: %s", ringServer, err)
+			sleeper()
+			continue
+		}
+		for {
+			select {
+			case <-exitChan:
+				break
+			default:
+			}
+			// TODO: This isn't going to get the right res type since it's not
+			// the right stream type yet.
+			res, err := stream.Recv()
+			if err != nil {
+				rs.logDebug("replValueStore: error with stream to ring service %s: %s", ringServer, err)
+				break
+			}
+			// TODO: Make use of ring once that's a thing in the res. Right
+			// now, just do some silliness to prototype the code.
+			if res != nil {
+				if res == nil {
+					// This will cache the ring if ringCachePath is not empty.
+					rs.SetRing(nil)
+					// Resets the exponential sleeper since we had success.
+					sleeperTicks = 2
+				}
+			}
+		}
+		conn.Close()
+		sleeper()
+	}
+}
+
+// Startup is not required to use the ReplValueStore; it will automatically
+// connect to backend stores as needed. However, if you'd like to use the ring
+// service to receive ring updates and have the ReplValueStore automatically
+// update itself accordingly, Startup will launch a connector to that service.
+// Otherwise, you will need to call SetRing yourself to inform the
+// ReplValueStore of which backends to connect to.
 func (rs *ReplValueStore) Startup(ctx context.Context) error {
+	rs.ringLock.Lock()
+	if rs.ringServerExitChan == nil {
+		rs.ringServerExitChan = make(chan struct{})
+		go rs.ringServerConnector(rs.ringServerExitChan)
+	}
+	rs.ringLock.Unlock()
 	return nil
 }
 
+// Shutdown will close all connections to backend stores and shutdown any
+// running ring service connector. Note that the ReplValueStore can still be
+// used after Shutdown, it will just start reconnecting to backends again. To
+// relaunch the ring service connector, you will need to call Startup.
 func (rs *ReplValueStore) Shutdown(ctx context.Context) error {
+	rs.ringLock.Lock()
+	if rs.ringServerExitChan != nil {
+		close(rs.ringServerExitChan)
+		rs.ringServerExitChan = nil
+	}
+	rs.storesLock.Lock()
+	for addr, stc := range rs.stores {
+		if err := stc.store.Shutdown(ctx); err != nil {
+			rs.logDebug("replValueStore: error during shutdown of store %s: %s", addr, err)
+		}
+		delete(rs.stores, addr)
+		select {
+		case <-ctx.Done():
+			rs.storesLock.Unlock()
+			return ctx.Err()
+		default:
+		}
+	}
+	rs.storesLock.Unlock()
+	rs.ringLock.Unlock()
 	return nil
 }
 
